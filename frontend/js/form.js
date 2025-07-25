4d839545078eded62cbcc93a7d66015f41c80d07
Yohanes Amelio Turnip <64017109+johanes147@users.noreply.github.com>
2025-07-25 03:03:22 -0700

    range slider for travel budget and accomodation budget

--- frontend/js/form.js
+++ frontend/js/form.js
@@ -31,3 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function formatRupiah(angka) {
+    return 'Rp' + angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
